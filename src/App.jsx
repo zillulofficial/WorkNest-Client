a14430ca8d400efe99d5a3ef97d5652b9ecbdaf4
zillulofficial <zillul.hakim100@gmail.com>
2025-02-18 19:49:18 +0600

    project setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-3xl font-semibold'>this is a demi thing</h2>
+      <button className="btn btn-dash btn-accent">Accent</button>
+    </>
+  )
+}
+
+export default App
